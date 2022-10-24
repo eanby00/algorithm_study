80bbc9a36aad3f9796c3962e0ee45b235440738b
eanby00 <wjdwnstjd00@gmail.com>
2022-10-24 19:11:00 +0900

    221024 solved 2475

---
+++ baekjoon_typescript/2475.js
@@ -0,0 +1,41 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://www.acmicpc.net/problem/2475
+const readline = __importStar(require("readline"));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input = '';
+rl.on("line", function (line) {
+    input = line.toString().trim();
+    rl.close();
+});
+rl.on("close", function () {
+    let numbers = input.split(" ").map(number => (Math.pow(parseInt(number), 2)));
+    let sum = numbers.reduce((accumulator, number) => accumulator + number) / 10;
+    console.log(sum);
+});
--- baekjoon_typescript/2475.ts
+++ baekjoon_typescript/2475.ts
@@ -1,10 +1,6 @@
-## baekjoon의 알고리즘 풀이 저장
-- typescript 주력으로 하루에 1~2문제씩 풀기
-- 문제 풀이를 통해 typescript 실력 향상과 사고력 향상을 노림
-- [solved.ac](https://solved.ac/class)의 클래스를 기준으로 진행할 예정
+// https://www.acmicpc.net/problem/2475
+import * as readline from "readline";
 
-### typescript에서의 input, output
-```
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -22,6 +18,3 @@ rl.on("close", function():void {
     let result: number = sum % 10;
     console.log(result)
 })
-```
-- input은 rl.on("line", ...)에서
-- output은 rl.on("close", ...)에서
