e94feccc5f5111a463d0473e83cfd2e98010291c
eanby00 <wjdwnstjd00@gmail.com>
2022-11-08 19:05:37 +0900

    Feat: "17219 비밀번호 찾기 구현"

---
+++ baekjoon_typescript/17219.js
@@ -0,0 +1,28 @@
+"use strict";
+// https://www.acmicpc.net/problem/17219
+// 비밀번호 찾기
+exports.__esModule = true;
+// 가설
+// n개의 주소와 비밀번호를 이용하여 map을 만든다.
+// 이후 m개의 주소에 대한 비밀번호를 출력한다.
+var fs = require("fs");
+var INPUT_LOCATION = 'inputs.txt';
+var inputs = fs
+    .readFileSync(INPUT_LOCATION)
+    .toString()
+    .trim()
+    .split('\n');
+var _a = inputs.map(function (input) {
+    return input.trim().split(' ');
+}), firstLine = _a[0], addressAndPasswords = _a.slice(1);
+var _b = firstLine.map(function (item) { return parseInt(item); }), n = _b[0], m = _b[1];
+var settingPasswords = addressAndPasswords.slice(0, n);
+var findPasswordByAddress = addressAndPasswords.slice(n);
+var addressToPassword = new Map();
+settingPasswords.forEach(function (addressAndPassword) {
+    var address = addressAndPassword[0], password = addressAndPassword[1];
+    addressToPassword.set(address, password);
+});
+findPasswordByAddress.forEach(function (address) {
+    console.log(addressToPassword.get(address[0]));
+});
---
+++ baekjoon_typescript/17219.ts
@@ -0,0 +1,36 @@
+// https://www.acmicpc.net/problem/17219
+// 비밀번호 찾기
+
+// 가설
+// n개의 주소와 비밀번호를 이용하여 map을 만든다.
+// 이후 m개의 주소에 대한 비밀번호를 출력한다.
+
+import * as fs from 'fs';
+
+const INPUT_LOCATION = 'inputs.txt';
+
+const inputs: string[] = fs
+  .readFileSync(INPUT_LOCATION)
+  .toString()
+  .trim()
+  .split('\n');
+
+const [firstLine, ...addressAndPasswords] = inputs.map((input) =>
+  input.trim().split(' ')
+);
+
+const [n, m] = firstLine.map((item) => parseInt(item));
+
+const settingPasswords = addressAndPasswords.slice(0, n);
+const findPasswordByAddress = addressAndPasswords.slice(n);
+
+const addressToPassword = new Map<string, string>();
+
+settingPasswords.forEach((addressAndPassword) => {
+  const [address, password] = addressAndPassword;
+  addressToPassword.set(address, password);
+});
+
+findPasswordByAddress.forEach((address) => {
+  console.log(addressToPassword.get(address[0]));
+});
